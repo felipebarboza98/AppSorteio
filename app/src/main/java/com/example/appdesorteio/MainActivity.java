e2ed519a2e1d1928a6bf1128dc129b8b355a2eaf
felipebarboza98 <felipef.barboza@hotmail.com>
2021-01-29 20:16:45 -0300

    Init

---
+++ app/src/main/java/com/example/appdesorteio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.appdesorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int bound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view) {
+
+        TextView texto = findViewById(R.id.testoResultado);
+
+        int x = new Random().nextInt (11);
+
+        texto.setText( "Número: " + x);
+    }
+
+}
+
+
